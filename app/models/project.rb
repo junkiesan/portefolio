7a47ab0dbfefd9dd909bf27c63f8ac6b7b3c5523
Julien Bouland <bouland.julien@gmail.com>
2020-07-20 14:04:58 +0200

    project model done

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  # Relations
+  has_many :languages, through: :language_projects
+  # ActiveStorage
+  has_one_attached :photo
+  # Validations
+  validates :title, :description, :url, presence: true
 end
