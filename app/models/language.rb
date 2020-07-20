14171695e5155839d4f5e9edc667a3106dd0084e
Julien Bouland <bouland.julien@gmail.com>
2020-07-20 17:08:05 +0200

    languages controller created

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,8 @@
 class Language < ApplicationRecord
+  # Relations
+  has_many :projects, through: :language_projects
+  # ActiveStorage
+  has_one_attached :photo
+  # Validations
+  validates :photo, presence: true
 end
