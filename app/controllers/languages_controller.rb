f9dadac8bfb0a35c6e8f56ecc8590df722081e90
Julien Bouland <bouland.julien@gmail.com>
2020-07-20 17:10:34 +0200

    languages controlller implemented with method show

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,11 @@
 class LanguagesController < ApplicationController
+  def show
+    @language = Language.find(params[:id])
+  end
+
+private
+
+  def languages_params
+    params.require(:language).permit(:name, :photo)
+  end
 end
