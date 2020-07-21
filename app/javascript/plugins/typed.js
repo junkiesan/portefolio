fcfd7770eae47ec5d29eb2107a891009e83d348e
Julien Bouland <bouland.julien@gmail.com>
2020-07-21 22:30:21 +0200

    time adjusted on typed.js and presntation upgraded

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -3,7 +3,7 @@ import Typed from "typed.js";
 const homeType = () => {
   if (document.querySelector('.element')){
     const typed = new Typed('.element', {
-      strings: ["Julien Bouland", "un dévelopeur fullstack ruby", "à la recherche d’un CDI", "un publicitaire", "un voyageur"],
+      strings: ["Julien Bouland", "un développeur fullstack ruby", "à la recherche d’un CDI", "un publicitaire", "un voyageur"],
       typeSpeed: 50,
       backSpeed: 50,
       loop: true
